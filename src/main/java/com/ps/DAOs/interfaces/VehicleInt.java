b73862eba59670b80758c2e92ec7aaa9a896658d
Marshall <mellisgibbs@gmail.com>
2024-06-14 16:27:48 -0400

    added methods to 'VehicleInt' interface

--- src/main/java/com/ps/DAOs/interfaces/VehicleInt.java
+++ src/main/java/com/ps/DAOs/interfaces/VehicleInt.java
@@ -1,4 +1,13 @@
 package com.ps.DAOs.interfaces;
 
+import com.ps.models.Vehicle;
+import java.util.List;
+
 public interface VehicleInt {
+    List<Vehicle> getVehiclesByPrice(double min, double max);
+    List<Vehicle> getVehiclesByMakeModel(String make, String model);
+    List<Vehicle> getVehiclesByYear(int min, int max);
+    List<Vehicle> getVehiclesByColor(String color);
+    List<Vehicle> getVehiclesByMileage(int min, int max);
+    List<Vehicle> getVehiclesByType(String inputVehicleType);
 }
